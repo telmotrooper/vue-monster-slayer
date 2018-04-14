7993fbc2e1b723ad768c6a95052a98789903cdf9
Telmo "Trooper <telmo.trooper@gmail.com>
2018-04-14 20:17:14 -0300

    Hiding/showing divs

--- js/app.js
+++ js/app.js
@@ -2,7 +2,13 @@ window.onload = function() {
 	new Vue({
 		el: "#app",
 		data: {
-
+			gameRunning: false
+		},
+		methods: {
+			newGame: function() {
+				console.log("Starting a new game...");
+				this.gameRunning = true;
+			}
 		}
 	});
 };
